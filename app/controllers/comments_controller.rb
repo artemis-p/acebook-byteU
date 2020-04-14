e8ab8f2917feef8457e545a0deb0a0c2876a0f5b
sgneha <sgneha05@gmail.com>
2020-04-14 18:08:12 +0100

    OB
    - only login user can write post
    - user can delete and update only his own post
    - user can edit only his own post
    
    - user can delete your comments from others posts
    - user can delete  comments made by others on your post
    - removed title from table and view
    - added user_id as foreign key in comments table
    - removed commenter box and added commented by: email of the user
    - added user email and created time in each post
    - post that is updated last will appear first
    - post can be updated(edited) only after 10 min of creation(flash msg)
    - In comments controller authentication added as nothing can be done unless loggedIn
    - comment belongs_to post,post belongs_to user
    - user has_many posts,user has_many comments,post has_many comments
    - line breaks in post text included
    
     '#' will be ignored, and an empty message aborts the commit.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,12 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
+    @comment.user_id=current_user.id
+    @comment.save
     redirect_to post_path(@post)
+    flash[:alert]="Comment created successfully"
   end
  
   def destroy
@@ -14,6 +18,6 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:body)
     end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,14 @@
 class PostsController < ApplicationController
-  
+  before_action :authenticate_user!,except: [:show,:index]
   def index
     @posts = Post.all.order(:updated_at).reverse_order
   end
 
   def show
     @post = Post.find(params[:id])
+    @post.comments
+    user_id = @post.user_id
+    @email= User.find(user_id).email
   end
 
   def new
@@ -18,21 +21,27 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-
-   if @post.save
-    redirect_to @post
-   else
-    render 'new'
-   end
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to @post
+    else
+     render 'new'
+    end
   end
 
   def update
     @post = Post.find(params[:id])
-
-    if @post.update(post_params)
-      redirect_to @post
+    post_time=@post.created_at
+    time_diff=Time.now-post_time
+    if (time_diff < 600)
+      if @post.update(post_params)
+        redirect_to @post
+      else
+        render 'edit'
+      end
     else
-      render 'edit'
+      redirect @post
+      flash[:alert]="Cannot update after 10 minutes of creation"
     end
   end
 
@@ -45,7 +54,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :text)
+    params.require(:post).permit(:text)
   end
 
 end
