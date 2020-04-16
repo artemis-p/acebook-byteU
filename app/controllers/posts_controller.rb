e4ca7dbf9e1e97136589ddf0072856543a376d16
sgneha <sgneha05@gmail.com>
2020-04-16 14:48:35 +0100

    corrected edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class PostsController < ApplicationController
-  before_action :authenticate_user!,except: [:show,:index]
+  before_action :authenticate_user!, except: %i[show index]
   def index
     @posts = Post.all.order(:updated_at).reverse_order
   end
@@ -8,16 +10,17 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @post.comments
     user_id = @post.user_id
-    @email= User.find(user_id).email
+    @email = User.find(user_id).email
+    @wall_id = @post.wall_id
   end
 
   def new
     @wall_id = params[:wall_id]
-    @post=Post.new
+    @post = Post.new
   end
 
   def edit
-    @post=Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def create
@@ -26,23 +29,23 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to @post
     else
-     render 'new'
+      render 'new'
     end
   end
 
   def update
     @post = Post.find(params[:id])
-    post_time=@post.created_at
-    time_diff=Time.now-post_time
-    if (time_diff < 600)
-      if @post.update(post_params)
+    post_time = @post.created_at
+    time_diff = Time.now - post_time
+    if time_diff < 600
+      if @post.update(post_params_text)
         redirect_to @post
       else
         render 'edit'
       end
     else
-      redirect @post
-      flash[:alert]="Cannot update after 10 minutes of creation"
+      redirect_to @post
+      flash[:alert] = 'Cannot update after 10 minutes of creation'
     end
   end
 
@@ -54,8 +57,12 @@ class PostsController < ApplicationController
   end
 
   private
+
   def post_params
     params.require(:post).permit(:text, :wall_id)
   end
 
+  def post_params_text
+    params.require(:post).permit(:text)
+  end
 end
