4d6b42678e6112d4106bacfddda477764ae50a9d
James Oddy <jamesoddy8@gmail.com>
2020-04-17 10:20:02 +0100

    two tests passing

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+
+# it 'user can create new post' do
+#   current_user
+#   visit "/wall/1"
+#   click_link 'Write a new post'
+#   # expect(page).to have_title("What's new with you?")
+#   # fill_in "Text", :with => "This is my first test post"
+#   # click_button "Create Post"
+#   # expect(page).to have_text("This is my first test post")
+# end
+# it 'user can update post'
+# it 'user can only update within 10 minutes'
+# it 'user can only update their own posts'
+# it 'user can delete post'
+# it 'user can only delete their own posts'
+# it 'displays a new post'
+# it 'posts are displayed in reverse order'
+# end
+end 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @user = User.new(email: 'example@example.com',
+            password: 'password', password_confirmation: 'password')
+  end
+  it 'should work with factory bot' do
+    user1 = create(:user)
+    expect(user1.valid?).to eq(true)
+  end
+  it 'email should be present' do
+   @user.email = ''
+   expect(@user.valid?).to eq(false)
+ end
 end
